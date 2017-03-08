9406e174b04a02125cff80d02c73fbb7899b562e
Dario Campisano <dario.campisano@gmail.com>
2017-03-08 15:39:30 +0100

    User class added instead of NutritionalFact

--- src/brainatwork/pattern/creational/builder/Main.java
+++ src/brainatwork/pattern/creational/builder/Main.java
@@ -2,6 +2,12 @@ package brainatwork.pattern.creational.builder;
 //
 public class Main {
 	public static void main(String[] argv){
-		NutritionalFact n = new NutritionalFact.Builder().sodium(10).carbo(15).fat(5).build();
+		 User u1 = new User.UserBuilder("Jhon", "Doe")
+		    .age(1520)
+		    .phone("1234567")
+		    .address("Fake address 1234")
+		    .build();
+		 
+		 System.out.println(u1.toString());
 	}
 }
