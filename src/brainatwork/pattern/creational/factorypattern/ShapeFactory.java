02103b76f5d9689c2a3b590f223a762f10f45f28
marsouf <marsouf224@gmail.com>
2018-03-27 10:50:51 +0100

    Updated [Shape brainatwork.pattern.creational.factorypattern.ShapeFactory#getShape(String)]

--- src/brainatwork/pattern/creational/factorypattern/ShapeFactory.java
+++ src/brainatwork/pattern/creational/factorypattern/ShapeFactory.java
@@ -4,16 +4,15 @@ public class ShapeFactory {
 	
 	   //use getShape method to get object of type shape 
 	   public Shape getShape(String shapeType){
-	      if(shapeType == null){
-	         return null;
-	      }		
-	      if(shapeType.equalsIgnoreCase("CIRCLE")){
-	         return new Circle();
-	      }    
-	      else if(shapeType.equalsIgnoreCase("SQUARE")){
-	         return new Square();
-	      }
-	      
-	      return null;
+		   if ("CIRCLE".equalsIgnoreCase(shapeType))
+		   {
+			   return new Circle();
+		   }
+		   else if ("SQUARE".equalsIgnoreCase(shapeType))
+		   {
+			   return new Square();
+		   }
+		   
+		   return null;
 	   }
 	}
